5ca8d929f042730b4447836f7ac6230bb90d3a00
kimchaewon <cheawon0319@gmail.com>
2024-10-30 16:50:05 -0700

    finish front end set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
 function App() {
-  const [count, setCount]= useState(0)
+  const [question, setQuestion]= useState("");
 
   return (
     <>
       <div className="containter">
         <h1>Open AI Developer API integration</h1>
+        <form>
+          <label className='form-label' htmlFor='txtQuestion'>Please Enter Your Question</label>
+          <input className='form-control' type='text' id='txtQuestion' name='txtQuestion' onChange={(evt) => setQuestion(evt.target.value)} />
+        </form>
+        <span>{question}</span>
       </div>
     </>
   )
