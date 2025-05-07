2b30d833e562073f0c53242ed2161b59bc794892
chaewon kim <cheawon0319@gmail.com>
2025-05-07 12:46:18 -0700

    install cors and hide url

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,27 @@
-import express from "express"
-import OpenAI from "openai"
+const express = require('express')
+const OpenAI = require('openai')
+const cors = require('cors')
 
 const config = require('./utils/config')
+
 
 const openai = new OpenAI({
     apiKey: config.OPENAI_API_KEY,
 })
-
 
 const app = express()
 
 // gpt-4o model does not exist -> try codes below to see my available models
 //const models = await openai.models.list()
 //console.log(models.data) // This will log all available models
-
+app.use(cors()) // allow cross-origin requests
 app.use(express.static('dist')) // 'use this landing page' to the backend (express)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // derive data that received from frontend
 
-// express route to support questions from the frontend
-app.post('/:userQuestion', async (req, res) => {
+
+
+app.post('/api/:userQuestion', async (req, res) => {
     var userQuestion = req.params.userQuestion
     // call open ai api
     const completion = await openai.chat.completions.create({
@@ -29,10 +31,13 @@ app.post('/:userQuestion', async (req, res) => {
         ],
     })
 
+    console.log(completion)
+
     res.json(completion.choices[0].message.content) // text data send back
 })
 
-app.listen(3000, () => {
-    console.log('Express Server running on http://localhost:3000')
+
+app.listen(config.PORT, () => {
+    console.log('Express Server running on http://' + config.ADDRESS + ':' + config.PORT)
 })
 
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 
+
+const PORT = process.env.PORT
+const ADDRESS = process.env.ADDRESS
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 
 module.exports = {
     OPENAI_API_KEY,
+    PORT,
+    ADDRESS,
 }
