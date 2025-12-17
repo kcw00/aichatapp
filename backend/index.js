8c7a85670e0a4ef9c16081e48b04938827771c29
chaewon kim <cheawon0319@gmail.com>
2025-12-17 01:04:09 -0800

    fix github actions workflow

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const OpenAI = require('openai')
-// const cors = require('cors') // CORS is now handled by nginx
 
 const config = require('./utils/config')
 
@@ -14,8 +13,6 @@ const app = express()
 // gpt-4o model does not exist -> try codes below to see my available models
 //const models = await openai.models.list()
 //console.log(models.data) // This will log all available models
-// CORS is now handled by nginx, no need for Express CORS middleware
-// app.use(cors(corsOptions))
 app.use(express.static('dist')) // 'use this landing page' to the backend (express)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // derive data that received from frontend
